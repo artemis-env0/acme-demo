fa6d0a0f22a4e7ba9f8316991d2d7613d2373a0a
Artem_is_env0 <artem.artyunov@env0.com>
2026-01-03 15:10:32 -0500

    Update outputs.tf

--- workflows/b1-compute/outputs.tf
+++ workflows/b1-compute/outputs.tf
@@ -9,3 +9,15 @@ output "instance_id" {
 output "network" {
   value = var.vpc_id
 }
+
+output "custom_flow_winner" {
+  value = try(trimspace(file("${path.module}/flow_winner.txt")), "flow_winner.txt not found")
+}
+
+output "project_custom_flow_marker" {
+  value = try(trimspace(file("${path.module}/project_flow_marker.txt")), "project_flow_marker.txt not found")
+}
+
+output "template_custom_flow_marker" {
+  value = try(trimspace(file("${path.module}/template_flow_marker.txt")), "template_flow_marker.txt not found")
+}
